b63b2e9609abe1bf16c2753171ad4d7297c7d674
Muzea001 <muat0072@gmail.com>
2024-11-11 09:34:50 +0100

    Setup DbContext

--- obj/Debug/net8.0/Interview_Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/Interview_Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interview_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ecea301ece9f987e3cd2c5241b80fa9ea968fd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9002078d51b10450ea247a8059f468fb881b052")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interview_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interview_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
