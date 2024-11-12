400e70de9fb796eabd83aa392e2bea3442c3b82f
Ali Haider Khan <ali_786@live.no>
2024-11-12 11:09:56 +0100

    setup for all controllers I will work on

---
+++ Controllers/LogbookController.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Interview_Server.Interfaces;
+using Interview_Server.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Interview_Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LogbookController : ControllerBase
+    {
+        private readonly IRepository<Logbook> _LogbookRepository;
+
+        public LogbookController(IRepository<Logbook> repository)
+        {
+            _LogbookRepository = repository;
+        }
+    }
+}
---
+++ Controllers/NoteController.cs
@@ -0,0 +1,18 @@
+﻿using Interview_Server.Interfaces;
+using Interview_Server.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Interview_Server.Controllers
+{
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+        private readonly IRepository<Note> _NoteRepository;
+
+        public NoteController(IRepository<Note> repository)
+        {
+            _NoteRepository = repository;
+        }
+
+    }
+}
