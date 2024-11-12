7053f7ac0a0781580a9fe09bfae19edb3bbf9070
Ali Haider Khan <ali_786@live.no>
2024-11-12 10:59:08 +0100

    setup for working on UserInterviewController

---
+++ Controllers/UserInterviewController.cs
@@ -0,0 +1,23 @@
+﻿using Interview_Server.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Interview_Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UserInterviewController : ControllerBase
+    {
+
+        private readonly IRepository<UserInterview> _UserInterviewRespository;
+
+        public UserInterviewController(IRepository<UserInterview> repository)
+        {
+
+           _UserInterviewRespository = repository;
+
+        }}
+
+    }
+
